c26a8ecd8080243140de074d54cbaa5bd776b708
mwleungac <mwleungac@oocl.com>
2020-04-08 23:42:45 +0800

    commit OutputResult

--- src/test/java/com/oocl/GuestNumberTest.java
+++ src/test/java/com/oocl/GuestNumberTest.java
@@ -10,15 +10,6 @@ public class GuestNumberTest {
 
     public int[] randomNumbers = {};
 
-    public static int findIndex(int arr[], int t)
-    {
-        int len = arr.length;
-        return IntStream.range(0, len)
-                .filter(i -> t == arr[i])
-                .findFirst() // first occurrence
-                .orElse(-1); // No element found
-    }
-
     public void generateRandomNumbers() {
         int numOfNumbersInSet = 4;
         int[] randomNumSet = new int[numOfNumbersInSet];
@@ -35,44 +26,61 @@ public class GuestNumberTest {
         randomNumbers = randomNumSet;
     }
 
+    public static int findIndex(int arr[], int t)
+    {
+        int len = arr.length;
+        return IntStream.range(0, len)
+                .filter(i -> t == arr[i])
+                .findFirst() // first occurrence
+                .orElse(-1); // No element found
+    }
+
     @Test
     public void OutputResult() {
 
         generateRandomNumbers();
 
         int[] randomNum = randomNumbers;
-        int[] guessingNum = {1,2,3,4};
+        int[] guessingNum = {1, 2, 3, 4};
         Integer correctNumbers = 0;
         Integer correctPositionAndNumber = 0;
 
-        System.out.println("Generated Numbers : " + Arrays.toString(randomNum));
-        System.out.println("Inputted Numbers : " + Arrays.toString(guessingNum));
+        if (guessingNum.length < 5) {
 
-        HashSet<Integer> set = new HashSet<Integer>();
-        for (int i = 0; i < randomNum.length; i++) {
+            System.out.println("Generated Numbers : " + Arrays.toString(randomNum));
+            System.out.println("Inputted Numbers : " + Arrays.toString(guessingNum));
 
-            if(findIndex(randomNum, randomNum[i])==findIndex(guessingNum, guessingNum[i])
-                    && randomNum[i] == guessingNum[i]){
-                correctPositionAndNumber = correctPositionAndNumber+1;
+            HashSet<Integer> set = new HashSet<Integer>();
+            for (int i = 0; i < randomNum.length; i++) {
+
+                if (findIndex(randomNum, randomNum[i]) == findIndex(guessingNum, guessingNum[i])
+                        && randomNum[i] == guessingNum[i]) {
+                    correctPositionAndNumber = correctPositionAndNumber + 1;
+                }
+
             }
 
-        }
+            for (int i = 0; i < randomNum.length; i++) {
 
-        for (int i = 0; i < randomNum.length; i++) {
+                for (int j = 0; j < guessingNum.length; j++) {
+                    if (randomNum[i] == guessingNum[j]) {
+                        set.add(randomNum[i]);
+                        correctNumbers = correctNumbers + 1;
 
-            for (int j = 0; j < guessingNum.length; j++) {
-                if (randomNum[i] == guessingNum[j] ){
-                    set.add(randomNum[i]);
-                    correctNumbers=correctNumbers+1;
-
+                    }
                 }
             }
+
+            correctNumbers = correctNumbers - correctPositionAndNumber;
+
+            System.out.println("No of correct num: " + correctNumbers);
+            System.out.println("No of correct positions and num: " + correctPositionAndNumber);
+            System.out.println("Result: " + correctPositionAndNumber + "A" + correctNumbers + "B");
         }
+        else {
 
-        correctNumbers = correctNumbers - correctPositionAndNumber;
-
-        System.out.println("No of correct num: " + correctNumbers);
-        System.out.println("No of correct positions and num: " + correctPositionAndNumber);
-        System.out.println("Result: " + correctPositionAndNumber + "A" + correctNumbers + "B");
+            System.out.println("Wrong Input");
+        }
     }
+
 }
