8c30b055d3d15e0781d282c086a3b72f890acf36
mwleungac <mwleungac@oocl.com>
2020-04-10 20:58:22 +0800

    commit should_return_4A0B

--- src/test/java/com/oocl/GuessNumTest.java
+++ src/test/java/com/oocl/GuessNumTest.java
@@ -29,4 +29,51 @@ public class GuessNumTest {
         Assert.assertEquals("1A0B", result);
     }
 
+    @Test
+    public void should_return_0A2B() {
+
+        String input = "1234";
+        String answer = "2478";
+
+        GuessNum guessNum = new GuessNum();
+        String result = guessNum.outputResult(input , answer);
+
+        Assert.assertEquals("0A2B", result);
+    }
+
+    @Test
+    public void should_return_1A2B() {
+
+        String input = "1234";
+        String answer = "0324";
+
+        GuessNum guessNum = new GuessNum();
+        String result = guessNum.outputResult(input , answer);
+
+        Assert.assertEquals("1A2B", result);
+    }
+
+    @Test
+    public void should_return_0A4B() {
+
+        String input = "1234";
+        String answer = "4321";
+
+        GuessNum guessNum = new GuessNum();
+        String result = guessNum.outputResult(input , answer);
+
+        Assert.assertEquals("0A4B", result);
+    }
+
+    @Test
+    public void should_return_4A0B() {
+
+        String input = "1234";
+        String answer = "1234";
+
+        GuessNum guessNum = new GuessNum();
+        String result = guessNum.outputResult(input , answer);
+
+        Assert.assertEquals("4A0B", result);
+    }
 }
