88f727154ec17829c93082a5dacb01b855ed5ae4
mwleungac <mwleungac@oocl.com>
2020-04-10 20:56:25 +0800

    commit should_return_1A0B

--- src/test/java/com/oocl/GuessNumTest.java
+++ src/test/java/com/oocl/GuessNumTest.java
@@ -17,5 +17,16 @@ public class GuessNumTest {
         Assert.assertEquals("0A0B", result);
     }
 
+    @Test
+    public void should_return_1A0B() {
+
+        String input = "1234";
+        String answer = "1567";
+
+        GuessNum guessNum = new GuessNum();
+        String result = guessNum.outputResult(input , answer);
+
+        Assert.assertEquals("1A0B", result);
+    }
 
 }
